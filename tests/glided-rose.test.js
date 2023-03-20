05d0858e562c59d22f1dcbe0472a96a542f4f423
empaid26 <empaid26@gmail.com>
2023-03-20 20:51:53 +0530

    setup project

---
+++ tests/glided-rose.test.js
@@ -0,0 +1,9 @@
+const { Item, GildedRose } = require('.././app/glided-rose');
+
+describe('Gilded Rose', () => {
+  it('should foo', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('fixme');
+  });
+});
