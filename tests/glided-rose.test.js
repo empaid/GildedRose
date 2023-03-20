5193fe1315910f6636778e3d889f5c6ccc7ae369
empaid26 <empaid26@gmail.com>
2023-03-20 23:22:50 +0530

    Refactor code

--- tests/glided-rose.test.js
+++ tests/glided-rose.test.js
@@ -40,7 +40,7 @@ describe('Gilded Rose', () => {
     expect(items[0].quality).toBe(2);
   });
 
-  it('should not increase quality more than 50"', () => {
+  it('should not increase quality more than 50', () => {
     const gildedRose = new GildedRose([new Item("Aged Brie", 2, 50)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toBe(1);
@@ -55,37 +55,35 @@ describe('Gilded Rose', () => {
   });
 
 
-  describe('"Backstage passes to a TAFKAL80ETC concert"', () => {
+  describe('"Backstage passes to a TAFKAL80ETC concert', () => {
     
-    it('should increase quality by 1 when SellIn is more than 10"', () => {
+    it('should increase quality by 1 when SellIn is more than 10', () => {
       const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 13, 5)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(12);
       expect(items[0].quality).toBe(6);
     });
 
-    it('should increase quality by 2 when SellIn is 10 or less"', () => {
+    it('should increase quality by 2 when SellIn is 10 or less', () => {
       const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 5)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(9);
       expect(items[0].quality).toBe(7);
     });
 
-    it('should increase quality by 3 when SellIn is more than 1"', () => {
+    it('should increase quality by 3 when SellIn is more than 1', () => {
       const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(4);
       expect(items[0].quality).toBe(8);
     });
 
-    it('should set quality to 0 when SellIn is more than 1"', () => {
+    it('should set quality to 0 when SellIn is more than 1', () => {
       const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5)]);
       const items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(-1);
       expect(items[0].quality).toBe(0);
     });
   });
-
-
-  
 });
+
